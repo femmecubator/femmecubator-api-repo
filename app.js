de5a0fe579460c13edb8356cda6a666c4ceb1de0
vuonga1103 <vuonga1103@yahoo.com>
2020-10-13 12:37:27 -0400

    created skeleton

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+
+app.get("/:name", (req, res) => {
+  const name = req.params.name;
+  res.json(`Hello ${name}`);
+ });
