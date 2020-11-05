e2037f27f838b91f77af518a03684dcfc9064626
vuonga1103 <vuonga1103@yahoo.com>
2020-11-05 14:32:43 -0500

    connected to mongodb

--- server/middleware/common-menu/index.js
+++ server/middleware/common-menu/index.js
@@ -1,14 +1,35 @@
 const { HttpStatusCodes } = require('../../utils/constants');
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr(process.env.SECRET_KEY);
+const MongoClient = require('mongodb').MongoClient;
 
 const commonMenuMiddleware = {
-    getMenuItems: (req, res) => {
-      // Checking if user is logged in
-      if (!req.session.userId) {
-        res.status(HttpStatusCodes.StatusCodes.FORBIDDEN)
-      } else {
-        // TO DO: Send back menu API
+  getMenuItems: (req, res) => {
+    const uri = cryptr.decrypt(process.env.MONGO_DB_URL);
+
+    MongoClient.connect(
+      uri,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      function (err, client) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        const db = client.db('femmecubatorDB');
+
+        db.collection('common-menu')
+          .find({})
+          .toArray(function (err, items) {
+            if (err) throw err;
+            res.json(items[0]);
+          });
       }
-    }
+    );
+  },
 };
 
 module.exports = commonMenuMiddleware;
