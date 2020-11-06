6befab4015b9ecf0a134317eb77e8482a96f17df
cfernando415 <42846522+cfernando415@users.noreply.github.com>
2020-11-06 10:53:16 -0500

    fake login that provides cookies (#6)
    * fake login that provides cookies
    
    * removed cors
    
    * update eslint rules, scripts, and cookie options

--- server/client/index.js
+++ server/client/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const greetingRoutes = require('../routes/greetings');
 const encryptRoutes = require('../routes/encrypt');
+const loginRoutes = require('../routes/login');
+
 module.exports = () => {
   const app = express();
   app.use('/api/greetings', greetingRoutes);
   app.use('/api/encrypt', encryptRoutes);
+  app.use('/api/login', loginRoutes);
   return app;
 };
