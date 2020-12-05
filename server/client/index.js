cb37302ce0a16757506c8535a5da495c884b740a
cfernando415 <ca.fernando415@gmail.com>
2020-12-05 00:54:49 -0500

    cors-fix

--- server/client/index.js
+++ server/client/index.js
@@ -4,9 +4,18 @@ const commonMenuRoutes = require('../routes/common-menu');
 const encryptRoutes = require('../routes/encrypt');
 const loginRoutes = require('../routes/login');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
+const corsOptions = {
+  allRoutes: true,
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+  methods: 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+  headers: 'content-type',
+};
 module.exports = () => {
   const app = express();
+  app.use(cors(corsOptions));
   app.use(cookieParser());
   app.use('/api/greetings', greetingRoutes);
   app.use('/api/commonMenu', commonMenuRoutes);
