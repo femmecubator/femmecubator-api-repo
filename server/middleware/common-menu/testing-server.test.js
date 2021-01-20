3796d83f4156f520132159c46c58d3a09397bcfb
Jackson Chen <53205189+PeaWarrior@users.noreply.github.com>
2021-01-19 21:44:38 -0500

    Memory server (#29)
    * initiate mockMongoClient
    
    * initiate mongoMockClient
    
    * create mongodb memory server, mock server data and test
    
    * clean up
    
    * mock decrypt url
    
    * remove vestigial var

---
+++ server/middleware/common-menu/testing-server.test.js
@@ -0,0 +1,55 @@
+const mongo = require('../../utils/__mocks__/mockMongoClient');
+const commonMenuMiddleware = require('.');
+const httpMocks = require('node-mocks-http');
+
+jest.mock('cryptr', () => {
+  const mockPlainText = process.env.MONGO_URL;
+  return jest.fn().mockImplementation(() => {
+    return { decrypt: () => mockPlainText };
+  });
+});
+
+describe('testing', () => {
+  const mockMongoClient = new mongo;
+
+  beforeAll(async () => {
+    process.env.COMMON_MENU_COLLECTION='common-menu'
+    await mockMongoClient.startAndPopulateDB();
+  });
+  afterAll(() => mockMongoClient.stop());
+
+  it('return common-menu API', async () => {
+    const req = httpMocks.createRequest({
+      method: 'GET',
+      url: '/api/common-menu',
+    });
+    const res = httpMocks.createResponse({
+      locals: {
+        user: {
+          email: 'jane_d@gmail.com',
+          userName: 'Jane D.',
+          role_id: 1,
+          title: 'Software Engineer'
+        },
+      },
+    });
+
+    const expectedResp = {
+      role_id: 1,
+      headers: [
+        { id: 1, label: 'What We Do', href: '/about' },
+        { id: 2, label: 'Threads', href: '/threads' },
+        { id: 3, label: 'Mentors', href: '/mentors' }
+      ],
+      utilities: [
+        { id: 1, label: 'Settings', color: 'inherit', href: '/settings' },
+        { id: 2, label: 'Log Out', color: 'inherit', href: '/login?logout=true' },
+      ],
+      userName: 'Jane D.',
+      title: 'Software Engineer'
+    }
+
+    await commonMenuMiddleware.getMenuItems(req, res);
+    expect(res._getData().data).toEqual(expectedResp);
+  })
+});
