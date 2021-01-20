7600ba28b8252a1bf7ce60a039c1b1227e374a20
PeaWarrior <jacksonchen171@gmail.com>
2021-01-19 19:30:10 -0500

    create mongodb memory server, mock server data and test

--- server/middleware/common-menu/testing-server.test.js
+++ server/middleware/common-menu/testing-server.test.js
@@ -1,50 +1,60 @@
-const mongo = require('./__mocks__/mockMongoClient');
+const mongo = require('../../utils/__mocks__/mockMongoClient');
+const commonMenuMiddleware = require('.');
+const httpMocks = require('node-mocks-http');
+
+jest.mock('cryptr', () => {
+  const mockPlainText = 'mongodb://fakeUser:fakePassword@mongodb.fakeDomain.com:27017/fakeDb';
+  return jest.fn().mockImplementation(() => {
+    return { decrypt: () => mockPlainText };
+  });
+});
  
 describe('testing', () => {
   const mockMongoClient = new mongo;
 
   beforeAll(async () => {
+    process.env.COMMON_MENU_COLLECTION='common-menu'
     await mockMongoClient.startAndPopulateDB();
   });
   afterAll(() => mockMongoClient.stop());
 
-    it('should insert a doc into collection', async () => {
-    const users = mockMongoClient.db.collection('users');
-   
-    const mockUser = {_id: '123', name: 'Billy'};
-    await users.insertOne(mockUser);
-   
-    const insertedUser = await users.findOne({_id: '123'});
-    console.log(await users.find().toArray())
-    expect(insertedUser).toEqual(mockUser);
-  });
+  it('return common-menu API', async () => {
+    const req = httpMocks.createRequest({
+      method: 'GET',
+      url: '/api/common-menu',
+    });
+    const res = httpMocks.createResponse({
+      locals: {
+        user: {
+          email: 'jane_d@gmail.com',
+          userName: 'Jane D.',
+          role_id: 1,
+          title: 'Software Engineer'
+        },
+      },
+    });
+
+    const expectedResp = {
+      role_id: 1,
+      headers: [
+        { id: 1, label: 'What We Do', href: '/about' },
+        { id: 2, label: 'Threads', href: '/threads' },
+        { id: 3, label: 'Mentors', href: '/mentors' }
+      ],
+      utilities: [
+        { id: 1, label: 'Settings', color: 'inherit', href: '/settings' },
+        {
+          id: 2,
+          label: 'Log Out',
+          color: 'inherit',
+          href: '/login?logout=true'
+        }
+      ],
+      userName: 'Jane D.',
+      title: 'Software Engineer'
+    }
+
+    await commonMenuMiddleware.getMenuItems(req, res);
+    expect(res._getData().data).toEqual(expectedResp);
+  })
 });
-
-// describe('insert', () => {
-//   let connection = mockMongoUtil.createMemoryServer();
-//   console.log(connection);
-//   // let db;
- 
-//   beforeEach(() => {
-
-//   })
-//   // beforeAll(async () => {
-//   //   db = await connection.db();
-//   // });
- 
-//   afterAll(async () => {
-//     await connection.close();
-//   });
-
-//   it('should insert a doc into collection', async () => {
-//     // const users = db.collection('users');
-//     const users = 'hello'
-   
-//     const mockUser = {_id: 'some-user-id', name: 'John'};
-//     // await users.insertOne(mockUser);
-   
-//     // const insertedUser = await users.findOne({_id: 'some-user-id'});
-//     // expect(insertedUser).toEqual(mockUser);
-//     expect('hello').toEqual('hello')
-//   });
-// });
--- server/utils/mongoUtil.js
+++ server/utils/mongoUtil.js
@@ -1,21 +1,16 @@
 const MongoClient = require('mongodb').MongoClient;
 const Cryptr = require('cryptr');
-// const {
-//   MockMongoClient,
-// } = require('../middleware/common-menu/__mocks__/mockMongoClient');
-const { SECRET_KEY, MONGO_DB_URL, FEMMECUBATOR_DB, MOCK_TEST } = process.env;
+const { SECRET_KEY, MONGO_DB_URL, FEMMECUBATOR_DB, MONGO_URL } = process.env;
 const cryptr = new Cryptr(SECRET_KEY);
-const url = cryptr.decrypt(MONGO_DB_URL);
+const url = MONGO_URL || cryptr.decrypt(MONGO_DB_URL);
 
 module.exports = {
   fetchCollection: async function (collectionName) {
-    // const mongoClient = MOCK_TEST === 'true' ? MockMongoClient : MongoClient;
-
     const client = await MongoClient.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
+    
     const db = client.db(FEMMECUBATOR_DB);
     const collectionObj = db.collection(collectionName);
     return collectionObj;
