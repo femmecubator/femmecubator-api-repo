42c133caf74bcfd7ab1f29446e859871e569ce0e
Jackson Chen <53205189+PeaWarrior@users.noreply.github.com>
2021-01-27 18:46:49 -0500

    Directory api (#40)
    * initiate mentor route and middleware
    
    * created /api/mentors route
    
    * initiate getMentors test
    
    * update middleware to return new user model data
    
    * add drop and dropMentors to mockMongoUtil
    
    * add test for status 200, 404 and 400
    
    * clean up
    
    * drop collections after each test
    
    * add 504 gateway test
    
    * destructure StatusCodes
    
    Co-authored-by: AlveeM <alvee.akand@outlook.com>

--- server/utils/__mocks__/mockMongoClient.js
+++ server/utils/__mocks__/mockMongoClient.js
@@ -8,5 +8,25 @@ module.exports = {
 
     await usersCollection.insertMany(users);
     await commonMenuCollection.insertMany(commonMenu);
+  },
+  drop: async function(client, collection) {
+    const db = client.db();
+    if (collection) {
+      db.collection(collection).drop();
+    } else {
+      const collectionsObj = await db.listCollections().toArray();
+      const collections = collectionsObj.map(collection => collection.name);
+  
+      for(let i = 0; i < collections.length; i++) {
+        await db.collection(collections[i]).drop();
+      }
+    }
+  },
+  dropMentors: async function(client) {
+    const db = client.db();
+    const usersCollection = db.collection('users');
+    const mentorQuery = { "role_id": { "$eq": 0 } };
+
+    await usersCollection.deleteMany(mentorQuery);
   }
 };
