952cca1145f6559b809ae411acff4cdddd838d1d
“DonatelloTHM” <donat.pllana@outlook.com>
2021-03-03 17:34:08 -0500

    updated the logic inside login route to use the middleware

--- server/routes/login/index.js
+++ server/routes/login/index.js
@@ -1,35 +1,11 @@
 const express = require('express');
 const timeout = require('connect-timeout');
 const router = express.Router();
-const { TIMEOUT, HttpStatusCodes } = require('../../utils/constants');
-const JWT = require('jsonwebtoken');
-const { uuid } = require('uuidv4');
+const { TIMEOUT } = require('../../utils/constants');
+const loginMiddleware = require('../../middleware/login');
 
 router.use(express.json());
 
-router.post('/', timeout(TIMEOUT, { respond: true }), (req, res) => {
-  // will put logic in middleware later
-  // stub login api that responds with a cookie value
-  const { email, userName } = req.body;
-  const payload = {
-    email,
-    userName: 'jane.doe',
-    role_id: 1,
-    title: 'Software Engineer',
-  };
-  const cookieExp = new Date(Date.now() + 8 * 3600000);
-  const options = {
-    expires: cookieExp,
-    path: '/',
-    domain: process.env.DOMAIN || 'femmecubator.com',
-  };
-  const token = JWT.sign(payload, process.env.SECRET_KEY);
-
-  res
-    .status(HttpStatusCodes.StatusCodes.OK)
-    .cookie('TOKEN', token, options)
-    .cookie('SESSIONID', uuid(), options)
-    .end();
-});
+router.post('/', timeout(TIMEOUT, { respond: true }), loginMiddleware.login);
 
 module.exports = router;
