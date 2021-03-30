431827493e8c778079b4fcf6f6c2378cc46bd2b6
cfernando415 <42846522+cfernando415@users.noreply.github.com>
2021-03-30 18:48:37 -0400

    Update app.js
    testing auto deployment

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('dotenv').config({ silent: true });
 const app = require('./client');
 const { PORT } = require('./utils/constants');
 const logger = require('simple-node-logger').createSimpleLogger();
-// Testing UAT
+
 app.server = app().listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
