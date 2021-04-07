098873a5aba3cbade7107fdbe77bb23a55920396
Netaly <netaly.ramirez@gmail.com>
2021-04-06 21:18:20 -0400

    added more mentors to mockAPI

--- server/middleware/directory/index.js
+++ server/middleware/directory/index.js
@@ -15,7 +15,7 @@ const mentorsDirectory = [
   {
     firstName: 'Bruce',
     lastName: 'Banner',
-    title: 'Coding Mentor',
+    title: 'UI/UX',
     mentorSkills:
       'Wireframing, Prototyping, User Research, Customer Journey, Persona',
     bio:
@@ -32,7 +32,29 @@ const mentorsDirectory = [
     bio:
       'Lorem ipsum testing lines limited to an awesomely composed lead sentence or series of posts. Snappy, judgy, tyrannical. Compassionate or narcissistic. It is a placeholderawesomely composed lead sentence or series of posts. Snappy, judgy, tyrannical. Compassionate or narcissistic.Snappy, judgy, tyrannical. Compassionate or narcissistic. It is a placeholder  limited to 280 char text to a max of 5 Compassionate or narcissistic. It is a placeholder  limited to 280 char text to a max of 5 ',
     initials: 'CF',
-    _id: 'cbvnflkjf4',
+    _id: 'cbvnflvvvkjf4',
+  },
+  {
+    firstName: 'Thor',
+    lastName: 'Odison',
+    title: 'UI/UX',
+    mentorSkills:
+      'God of Thunder, Son of Odison, User Research, Customer Journey, Persona',
+    bio:
+      'Lorem ipsum testing lines limited to an awesomely composed lead sentence or series of posts. Snappy, judgy, tyrannical. Compassionate or narcissistic. It is a placeholderawesomely composed lead sentence or series of posts. Snappy, judgy, tyrannical. Compassionate or narcissistic.Snappy, judgy, tyrannical. Compassionate or narcissistic. It is a placeholder  limited to 280 char text to a max of 5 Compassionate or narcissistic. It is a placeholder  limited to 280 char text to a max of 5 ',
+    initials: 'TO',
+    _id: 'cb123vnflkjf4',
+  },
+  {
+    firstName: 'Peter',
+    lastName: 'Parker',
+    title: 'Coding Mentor',
+    mentorSkills:
+      'Hero, Friendly Neighbor, User Research, Customer Journey, Persona',
+    bio:
+      'Lorem ipsum testing lines limited to an awesomely composed lead sentence or series of posts. Snappy, judgy, tyrannical. Compassionate or narcissistic. It is a placeholderawesomely composed lead sentence or series of posts. Snappy, judgy, tyrannical. Compassionate or narcissistic.Snappy, judgy, tyrannical. Compassionate or narcissistic. It is a placeholder  limited to 280 char text to a max of 5 Compassionate or narcissistic. It is a placeholder  limited to 280 char text to a max of 5 ',
+    initials: 'PP',
+    _id: 'cbvnflkasdjf4',
   },
   
 ];
