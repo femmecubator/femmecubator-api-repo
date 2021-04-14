8b6d5e1277d454b97e134dff61f775dec7fcbea3
PeaWarrior <jacksonchen171@gmail.com>
2021-04-13 23:09:55 -0400

    change mongoUtil to class, fix async test issue, remove unused files

--- server/utils/__mocks__/mockMongoClient.js
+++ server/utils/__mocks__/mockMongoClient.js
@@ -2,28 +2,27 @@ const { users, commonMenu } = require('./mockData');
 
 module.exports = {
   seed: async function(client) {
-    const db = client.db();
+    const db = client.db;
     const usersCollection = db.collection('users');
     const commonMenuCollection = db.collection('common-menu');
 
     await usersCollection.insertMany(users);
     await commonMenuCollection.insertMany(commonMenu);
   },
-  drop: async function(client, collection) {
-    const db = client.db();
-    if (collection) {
-      db.collection(collection).drop();
-    } else {
-      const collectionsObj = await db.listCollections().toArray();
-      const collections = collectionsObj.map(collection => collection.name);
-  
-      for(let i = 0; i < collections.length; i++) {
-        await db.collection(collections[i]).drop();
-      }
+  drop: async function(client) {
+    const db = client.db;
+    const collectionsObj = await db.listCollections().toArray();
+
+    for(let i = 0; i < collectionsObj.length; i++) {
+      await db.collection(collectionsObj[i].name).deleteMany();
     }
   },
+  dropDatabase: async function(client) {
+    const db = client.db;
+    await db.dropDatabase();
+  },
   dropMentors: async function(client) {
-    const db = client.db();
+    const db = client.db;
     const usersCollection = db.collection('users');
     const mentorQuery = { "role_id": { "$eq": 0 } };
 
