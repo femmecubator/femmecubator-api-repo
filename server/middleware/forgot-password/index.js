c1e62c2eed12a56e3192231e2dfde865b93dd741
PeaWarrior <jacksonchen171@gmail.com>
2021-04-16 19:22:01 -0400

    add forgotPassword route and findUserByEmail

--- server/middleware/forgot-password/index.js
+++ server/middleware/forgot-password/index.js
@@ -1,5 +1,28 @@
+const mongoUtil = require('../../utils/mongoUtil');
+const { USERS_COLLECTION } = process.env;
+
+const findUserByEmail = async ({ body: { email } }) => {
+  const usersCollection = await mongoUtil.fetchCollection(USERS_COLLECTION);
+  const user = await usersCollection.findOne({ email: email });
+  return user;
+};
+
+const forgotPassword = async (req, res) => {
+  try {
+    const user = await findUserByEmail(req);
+    if (user) {
+      console.log('user exists', user);
+    } else {
+      console.log('user does not exist', user);
+    }
+  } catch (error) {
+    // handle errors
+  }
+}
+
 const forgotPasswordMiddleware = {
-  forgotPassword: (req, res) => {
+  forgotPassword: async (req, res) => {
+    await forgotPassword(req, res);
     res.send('forgot password');
   },
 };
--- server/routes/forgot-password/index.js
+++ server/routes/forgot-password/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const forgotPasswordMiddleware = require('../../middleware/forgot-password');
 
+router.use(express.json());
 router.post('/', forgotPasswordMiddleware.forgotPassword);
 
 module.exports = router;
