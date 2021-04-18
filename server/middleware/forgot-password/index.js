2f577a07191d4bc91e14a98a779e00eceae16824
PeaWarrior <jacksonchen171@gmail.com>
2021-04-18 14:44:09 -0400

    add findUserAndSetResetToken

--- server/middleware/forgot-password/index.js
+++ server/middleware/forgot-password/index.js
@@ -1,15 +1,35 @@
+const crypto = require('crypto');
 const mongoUtil = require('../../utils/mongoUtil');
 const { USERS_COLLECTION } = process.env;
 
-const findUserByEmail = async ({ body: { email } }) => {
+const findUserAndSetResetToken = async ({ body: { email } }, resetPasswordToken, resetPasswordDate) => {
+  const query = { email: email };
+  const update = {
+    $set: {
+      resetPasswordToken: resetPasswordToken,
+      resetPasswordDate: resetPasswordDate,
+    }
+  };
+  const options = { returnOriginal: false };
+
   const usersCollection = await mongoUtil.fetchCollection(USERS_COLLECTION);
-  const user = await usersCollection.findOne({ email: email });
-  return user;
+  const { value } = await usersCollection.findOneAndUpdate(query, update, options);
+  return value;
 };
+
+const generateRandomToken = () => {
+  const buf = crypto.randomBytes(20);
+  const token = buf.toString('hex');
+  return token;
+}
 
 const forgotPassword = async (req, res) => {
   try {
-    const user = await findUserByEmail(req);
+    const resetPasswordToken = generateRandomToken();
+    const resetPasswordDate = Date.now() + 3600000;
+
+    const user = await findUserAndSetResetToken(req, resetPasswordToken, resetPasswordDate);
+
     if (user) {
       console.log('user exists', user);
     } else {
