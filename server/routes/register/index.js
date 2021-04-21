28d8aa5c085884d38a4331d9a59bc8c4207dd97c
ccedacero(Cristian Cedacero) <44513825+ccedacero@users.noreply.github.com>
2021-04-20 20:10:55 -0400

    Add registration (#36)
    * current registratation api
    
    * init registration
    
    * remove extra mongo db dependency
    
    * current state
    
    * update dir
    
    * pr changes and updated mocks
    
    * rename registrationService to registrationMiddleware and cleanup
    
    * add validateFormFields and logValidateFormFields to registration middleware
    
    * initiate with MongoDB Memory Server
    
    * refactor validateFormFields and registrationLogger
    
    * refactor validateFormFields to isFormValid and hashForm; cleanup
    
    * add status 200 test
    
    * fix 200 test
    
    * add 400 and 504 tests
    
    * add email in use validation
    
    * add 409 test
    
    * clean up
    
    * add email validation
    
    * add invalid form field test
    
    * add registrationLogger to end of registration
    
    * minor cleanup
    
    * comment out role_id from validation and add role_id to hashForm
    
    * add default role_id in middleware if not provided
    
    * changed test role_id to 1
    
    * update userPayload
    
    * update token to include userName
    
    * clean up
    
    * fix role_id to int32 in userPayload
    
    * mock registration loggers in test
    
    * cleanup
    
    * update tests to use mongoUtil
    
    * refactor tests
    
    * clean up
    
    Co-authored-by: PeaWarrior <jacksonchen171@gmail.com>
    Co-authored-by: cfernando415 <42846522+cfernando415@users.noreply.github.com>

--- server/routes/register/index.js
+++ server/routes/register/index.js
@@ -1,49 +1,14 @@
 const express = require('express');
 const timeout = require('connect-timeout');
 const router = express.Router();
-const { TIMEOUT, HttpStatusCodes } = require('../../utils/constants');
+const { TIMEOUT } = require('../../utils/constants');
 const JWT = require('jsonwebtoken');
-const { uuid } = require('uuidv4');
-
+const registrationMiddleware = require('../../middleware/registration');
 router.use(express.json());
 
-router.post('/', timeout(TIMEOUT, { respond: true }), (req, res) => {
-  // will put logic in middleware later
-  // stub login api that responds with a cookie value
-  const {
-    firstName,
-    lastName,
-    prefLoc,
-    title,
-    email,
-    userName,
-    password,
-  } = req.body;
-
-  if (email.toLowerCase() === 'test1@femmecubator.com') {
-    res
-      .status(HttpStatusCodes.StatusCodes.UNAUTHORIZED)
-      .send({ err: { email: { message: 'Email already registered' } } });
-  } else if (userName.toLowerCase() === 'john.doe') {
-    res
-      .status(HttpStatusCodes.StatusCodes.UNAUTHORIZED)
-      .send({ err: { userName: { message: 'User name already taken' } } });
-  } else {
-    const payload = { email, userName, role_id: 1, title };
-    const cookieExp = new Date(Date.now() + 8 * 3600000);
-    const options = {
-      expires: cookieExp,
-      path: '/',
-      domain: process.env.DOMAIN || 'femmecubator.com',
-    };
-    const token = JWT.sign(payload, process.env.SECRET_KEY);
-
-    res
-      .status(HttpStatusCodes.StatusCodes.OK)
-      .cookie('TOKEN', token, options)
-      .cookie('SESSIONID', uuid(), options)
-      .end();
-  }
-});
-
+router.post(
+  '/',
+  timeout(TIMEOUT, { respond: true }),
+  registrationMiddleware.register
+);
 module.exports = router;
