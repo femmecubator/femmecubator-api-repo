bd246088bad001a4643035adc2aad7370ebc8e85
ccedacero(Cristian Cedacero) <44513825+ccedacero@users.noreply.github.com>
2021-04-20 20:10:55 -0400

    Add registration (#36)
    * current registratation api
    
    * init registration
    
    * remove extra mongo db dependency
    
    * current state
    
    * update dir
    
    * pr changes and updated mocks
    
    * rename registrationService to registrationMiddleware and cleanup
    
    * add validateFormFields and logValidateFormFields to registration middleware
    
    * initiate with MongoDB Memory Server
    
    * refactor validateFormFields and registrationLogger
    
    * refactor validateFormFields to isFormValid and hashForm; cleanup
    
    * add status 200 test
    
    * fix 200 test
    
    * add 400 and 504 tests
    
    * add email in use validation
    
    * add 409 test
    
    * clean up
    
    * add email validation
    
    * add invalid form field test
    
    * add registrationLogger to end of registration
    
    * minor cleanup
    
    * comment out role_id from validation and add role_id to hashForm
    
    * add default role_id in middleware if not provided
    
    * changed test role_id to 1
    
    * update userPayload
    
    * update token to include userName
    
    * clean up
    
    * fix role_id to int32 in userPayload
    
    * mock registration loggers in test
    
    * cleanup
    
    * update tests to use mongoUtil
    
    * refactor tests
    
    * clean up
    
    Co-authored-by: PeaWarrior <jacksonchen171@gmail.com>
    Co-authored-by: cfernando415 <42846522+cfernando415@users.noreply.github.com>

---
+++ server/middleware/registration/index.js
@@ -0,0 +1,123 @@
+const bcrypt = require('bcrypt');
+const {
+  HttpStatusCodes: { StatusCodes },
+} = require('../../utils/constants');
+const { OK, BAD_REQUEST, GATEWAY_TIMEOUT } = StatusCodes;
+const mongoUtil = require('../../utils/mongoUtil');
+const JWT = require('jsonwebtoken');
+const { v4 } = require('uuid');
+const registrationLogger = require('./registrationLogger');
+
+const resObj = (statusCode, message, data = {}) => ({
+  statusCode,
+  message,
+  data,
+});
+
+const isFormValid = ({ body }) => {
+  const formFields = [
+    // 'role_id',
+    'firstName',
+    'lastName',
+    'title',
+    'email',
+    'password'
+  ];
+  
+  for (let i = 0; i < formFields.length; i++) {
+    const field = formFields[i];
+    if (!Object.hasOwnProperty.call(body, formFields[i]) || body[field].length === 0) {
+      return false;
+    }
+  }
+
+  const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!emailPattern.test(body.email)) {
+    return false;
+  } else {
+    return true;
+  }
+
+};
+
+const hashForm = ({ body }) => {
+  const saltRounds = 10;
+  body.email = body.email.toLowerCase();
+  const { password, email, title, role_id, lastName, firstName } = body;
+  const userPayload = { password: bcrypt.hashSync(password, saltRounds), email, title, role_id: parseInt(role_id), lastName, firstName };
+  return userPayload;
+};
+
+const generateCookie = (res, userPayload) => {
+  const { DOMAIN, SECRET_KEY } = process.env;
+  const { email, role_id, firstName, lastName } = userPayload;
+  const cookieExp = new Date(Date.now() + 8 * 3600000);
+  const options = {
+    expires: cookieExp,
+    path: '/',
+    domain: DOMAIN || 'femmecubator.com',
+  };
+  const token = JWT.sign({email, role_id, userName: `${firstName} ${lastName[0]}.`}, SECRET_KEY);
+  res.cookie('TOKEN', token, options).cookie('SESSIONID', v4(), options);
+};
+
+const createNewUser = async (req, res) => {
+  const { USERS_COLLECTION, TEST_TIMEOUT } = process.env;
+  let data;
+  let statusCode;
+  let message;
+  let email;
+
+  try {
+    const userPayload = hashForm(req);
+    email = req.body.email;
+    
+    if (!isFormValid(req)) throw Error('Bad request');
+
+    const userCollection = await mongoUtil.fetchCollection(USERS_COLLECTION);
+    const userFound = await userCollection.findOne(
+      { email: email },
+      { projection: {email: 1} },
+    );
+    if (userFound) {
+      statusCode = 409;
+      throw Error('Email already in use');
+    }
+
+    const insertion = await userCollection.insertOne(userPayload);
+    const { _id, password, ...rest } = insertion.ops[0];
+    data = rest;
+    
+    if (!data || TEST_TIMEOUT) {
+      throw Error('Gateway Timeout');
+    } else {
+      generateCookie(res, userPayload);
+      statusCode = OK;
+      message = 'Success';
+      registrationLogger.success(email);
+    }
+  } catch (error) {
+    if (error.message !== 'Gateway Timeout') {
+      registrationLogger.error(error, email);
+      statusCode = statusCode || BAD_REQUEST;
+      message = error.message;
+    } else {
+      registrationLogger.timeout(email);
+      statusCode = GATEWAY_TIMEOUT;
+      message = 'Gateway Timeout';
+    }
+  } finally {
+    registrationLogger.end(email);
+  }
+  return resObj(statusCode, message, data);
+};
+
+const registrationMiddleware = {
+  register: async (req, res) => {
+    const { statusCode, ...rest } = await createNewUser(req, res);
+    res.status(statusCode).send(rest);
+  }
+};
+
+module.exports = registrationMiddleware;
+
---
+++ server/middleware/registration/registrationLogger.js
@@ -0,0 +1,43 @@
+const logger = require('simple-node-logger').createSimpleLogger();
+const { setLogDetails } = require('../../utils/constants');
+
+const registrationLogger = {
+  success: (email) => {
+    logger.info(
+      setLogDetails(
+        `registrationMiddleware.register`,
+        `User registration was successful`,
+        `email - ${email}`
+      )
+    );
+  },
+  end: (email) => {
+    logger.info(
+      setLogDetails(
+        `registrationMiddleware.register`,
+        `End of registration`,
+        `email - ${email}`
+      )
+    );
+  },
+  error: (error, email) => {
+    logger.error(
+      setLogDetails(
+        `registrationMiddleware.register`,
+        `${error.message}`,
+        `email - ${email}`
+      )
+    );
+  },
+  timeout: (email) => {
+    logger.error(
+      setLogDetails(
+        `registrationMiddleware.register`,
+        `Connection time out while registering new user`,
+        `email - ${email}`
+      )
+    );
+  }
+};
+
+module.exports = registrationLogger;
