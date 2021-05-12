a62f32a03b7bc5ce3aae0bb4f41d7eb78eab76b4
Netaly <netaly.ramirez@gmail.com>
2021-05-12 18:30:12 -0400

    updated test for login

--- server/middleware/login/login.test.js
+++ server/middleware/login/login.test.js
@@ -4,13 +4,13 @@ const mockMongoUtil = require('../../utils/__mocks__/mockMongoClient');
 const httpMocks = require('node-mocks-http');
 // create a crypter to make a fake key for a fake db
 jest.mock('cryptr', () => {
-    const mockPlainText = process.env.Mongo_URl;
+    const mockPlainText = process.env.MONGO_URL;
     return jest.fn().mockImplementation(() => {
         return { decrypt: () => mockPlainText};
     });
 });
 // create a fake logger for inputs
-jest.mock('../utils/authLogger', () => {
+jest.mock('../../utils/authLogger', () => {
     const authLogger = {
         end: jest.fn(),
         success: jest.fn(),
@@ -24,24 +24,22 @@ jest.mock('../utils/authLogger', () => {
 describe('loginMiddleware', () => {
     let request, response;
     const OLD_ENV = process.env;
-    const form = {
-        email: 'ali@gmail.com',
-        password: 'Abcd123!',
-    };
+   
     const returnResponse = {
         role_id: 1,
         firstName: "Ali",
         lastName: "Baba",
         hasOnboarded: false,
-        email: "ali@gmail.com",
+        email: "ALi@gmail.com",
         title: "Software Engineer",
         password: "Abcd123!",
-    }
+    };
 
     beforeAll(async () => {
+        await mongoUtil.init();
         process.env.USERS_COLLECTION = 'users';
         process.env.SECRET_KEY = 'ABC123';
-
+        await mockMongoUtil.seed(mongoUtil);
     });
 
     beforeEach(async () => {
@@ -53,25 +51,27 @@ describe('loginMiddleware', () => {
         });
     });
 
-    afterEach(async () => {
+    afterAll(async () => {
         await mockMongoUtil.drop(mongoUtil);
-    });
-
-    afterAll(() => {
         mongoUtil.close();
         process.env = OLD_ENV;
     });
 
-    test('Should Login Existing User, Return Cookie with Status 200', async () => {
-        const { email, password } = form;
+    test('Should Return Error With Invalid Credentials, Return Status 401', async () => {
+        const form = {
+            email: 'ali@gmail.com',
+            password: 'Abcd123!',
+        };
         request.body = form;
-        response.body = {
-            data: { email: email, password: password},
-            message: 'Success',
+
+        const expectedResp = {
+            data: {},
+            message: 'Wrong credentials',
         };
         await login(request, response);
-        const collectionObj = await mongoUtil.fetchCollection(process.env.USERS_COLLECTION);
-        console.log(collectionObj);
-        expect(response._getStatusCode()).toEqual(200);
+        expect(response._getStatusCode()).toEqual(401);
+        expect(response._getData()).toEqual(expectedResp);
+        expect(response.cookies.TOKEN).toBeUndefined();
     });
+
 });
