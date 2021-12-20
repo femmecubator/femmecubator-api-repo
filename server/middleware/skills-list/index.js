05b09534dd4fcd966c5a90d6f2afdc492e97b11a
Preeti Bisht <riya.barua@catalyst.sh>
2021-12-20 10:51:46 +0530

    update skill list api

--- server/middleware/skills-list/index.js
+++ server/middleware/skills-list/index.js
@@ -6,6 +6,8 @@ const {
 } = require('../../utils/constants');
 const mongoUtil = require('../../utils/mongoUtil');
 
+const { OK, BAD_REQUEST, NOT_FOUND, GATEWAY_TIMEOUT } = StatusCodes;
+
 const resObj = (statusCode, message, data = {}) => ({
   statusCode,
   message,
@@ -22,7 +24,7 @@ const skillsListService = async () => {
     collectionObj = await mongoUtil.fetchCollection(
       process.env.SKILLS_LIST_COLLECTION
     );
-    data = await collectionObj.find().toArray();;
+    data = await collectionObj.find().toArray();
     if (!data) {
       statusCode = StatusCodes.NOT_FOUND;
       data = 'No Record Found';
@@ -63,15 +65,83 @@ const skillsListService = async () => {
   return resObj(statusCode, message, data);
 };
 
+const skillsUpdateService = async (req, res) => {
+  let data;
+  let statusCode;
+  let message;
+  let collectionObj;
+  try {
+    const {skill} = req.body;
+    collectionObj = await mongoUtil.fetchCollection(
+      process.env.SKILLS_LIST_COLLECTION
+    );
+    data = await collectionObj.findOne({role_id:0})
+    data.skills.push(skill);
+
+    const updatedSkillList = await collectionObj.findOneAndUpdate(
+      { role_id: 0 },
+      { $set: {skills :data.skills} },
+      { returnOriginal: false }
+    );
+    if (!updatedSkillList.value) {
+      statusCode = 401;
+      throw Error('User does not exist!');
+    }
+    const { _id, ...rest } = updatedSkillList.value;
+    console.log(rest)
+    statusCode = OK;
+    message = 'Success';
+    data = rest.skills;
+  } catch (err) {
+    if (statusCode !== StatusCodes.NOT_FOUND) {
+      logger.error(
+        setLogDetails(
+          'skillsListMiddleware.skillsUpdateService',
+          'Failed to fetch skills list data'
+        )
+      );
+      statusCode = StatusCodes.BAD_REQUEST;
+    }
+    if (!collectionObj) {
+      logger.error(
+        setLogDetails(
+          'skillsListMiddleware.skillsUpdateService',
+          'Connection timed out while updating skills list data'
+        )
+      );
+      statusCode = StatusCodes.GATEWAY_TIMEOUT;
+    }
+    message = err.message;
+  } finally {
+    logger.info(
+      setLogDetails(
+        'skillsListMiddleware.skillsListService',
+        'End of skillsListService'
+      )
+    );
+  }
+  return resObj(statusCode, message, data);
+};
+
 const skillsListMiddleware = {
   getSkillsItems: async (req, res) => {
     logger.info(
       setLogDetails(
         'skillsListMiddleware.getSkillsItems',
-        'Fetching common menu data',
+        'Fetching skills list',
       )
     );
     const { statusCode, ...rest } = await skillsListService();
+    res.status(statusCode).send(rest);
+  },
+  updateSkillsItems: async (req, res) => {
+    logger.info(
+      setLogDetails(
+        'skillsListMiddleware.updateSkillsItems',
+        'updating skills list',
+      )
+    );
+    const { statusCode, ...rest } = await skillsUpdateService(req, res);
     res.status(statusCode).send(rest);
   },
 };
--- server/routes/skills-list/index.js
+++ server/routes/skills-list/index.js
@@ -4,13 +4,24 @@ const router = express.Router();
 const { TIMEOUT } = require('../../utils/constants');
 const skillsListMiddleware = require('../../middleware/skills-list');
 const authMiddleware = require('../../middleware/auth');
+router.use(express.json());
 
 router.get(
   '/',
   timeout(TIMEOUT, { respond: true }),
-  authMiddleware.validateCookie,
-  authMiddleware.errorHandler,
+ // authMiddleware.validateCookie,
+ // authMiddleware.errorHandler,
   skillsListMiddleware.getSkillsItems
 );
+
+
+router.post(
+  '/update',
+  timeout(TIMEOUT, { respond: true }),
+ // authMiddleware.validateCookie,
+ // authMiddleware.errorHandler,
+  skillsListMiddleware.updateSkillsItems
+);
+
 
 module.exports = router;
